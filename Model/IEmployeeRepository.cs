dc2731d901d78ec86c7c5f33f9cb3aeb0d1738e7
Bárbara_Marcello_02 <134644032+barbaram02@users.noreply.github.com>
2025-01-11 19:36:56 -0300

    Adicionei o construtor e a Interface em C#

--- Model/IEmployeeRepository.cs
+++ Model/IEmployeeRepository.cs
@@ -1,6 +1,10 @@
 ﻿namespace WebAPIC_.Model
 {
+    //Padrão Repository
     public interface IEmployeeRepository
     {
+        void Add(Employee employee);
+
+        List<Employee> GetAll();
     }
 }
